8ff832052300a1f7a0e1b6c092a2a46d6297d2c7
adham165 <adhamanas2004@gmail.com>
2023-12-25 21:51:24 +0200

    g

--- botRandom.h
+++ botRandom.h
@@ -3,7 +3,7 @@
 #include <time.h>
 // r1 c1 r2 c2 (c2==c1)&&(abs(r1-r2)==1) ,, (r2==r1)&&(abs(c1-c2)==1)
 //Function to generate a random number within a specific range [min, max]
-void  generateRandomNumber(int min, int max, int *r1, int *c1, int *r2, int *c2) // 1 10 // (10-1+1) + 1;
+    void generateRandomNumber(int min, int max, int *r1, int *c1, int *r2, int *c2) // 1 10 // (10-1+1) + 1;
     {
         //Seeding the random number generator with the current time
         time_t t;
