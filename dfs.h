1d411877870eecba585ddaa0fbaacba9057e099c
BadrGello <138599821+BadrGello@users.noreply.github.com>
2023-12-25 02:35:56 +0200

    v0.8 BETA
    Implemented
    1) Main Menu layout
    2) In-Game Menu layout
    3)Random Moves bot (still needs a bit of rework)

---
+++ dfs.h
@@ -0,0 +1,158 @@
+void dfs(int gameSize,char arr[gameSize*2+1][gameSize*2+1],int* r1,int* c1,int* r2,int* c2,int* z,int* moves,int* score){
+    if(*r1==*r2){
+        if(*r1!=gameSize+1 && arr[*r1+*r2-3][*c1+*c2-2]!=' '){
+            if(arr[*r1+*r2][*c1+*c2-2]==' '&&arr[*r1+*r2-1][*c1+*c2-1]!=' '&&arr[*r1+*r2-1][*c1+*c2-3]!=' '){
+             (*z)--;
+             (*moves)++;
+             (*score)++;
+             arr[*r1+*r2][*c1+*c2-2]=arr[*r1+*r2-2][*c1+*c2-2];
+             arr[*r1+*r2-1][*c1+*c2-2]=arr[*r1+*r2-3][*c1+*c2-2];
+             (*r1)++;
+             (*r2)++;
+             dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+            }
+            else if(arr[*r1+*r2][*c1+*c2-2]!=' '&&arr[*r1+*r2-1][*c1+*c2-1]==' '&&arr[*r1+*r2-1][*c1+*c2-3]!=' '){
+                 (*z)--;
+             (*moves)++;
+             (*score)++;
+             arr[*r1+*r2-1][*c1+*c2-1]=arr[*r1+*r2-2][*c1+*c2-2]-1;
+             arr[*r1+*r2-1][*c1+*c2-2]=arr[*r1+*r2-3][*c1+*c2-2];
+             (*r2)++;
+             if(*c1>*c2) *c2=*c1;
+             else *c1=*c2;
+             dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+            }
+            else if(arr[*r1+*r2][*c1+*c2-2]!=' '&&arr[*r1+*r2-1][*c1+*c2-1]!=' '&&arr[*r1+*r2-1][*c1+*c2-3]==' ') {
+                 (*z)--;
+             (*moves)++;
+             (*score)++;
+             arr[*r1+*r2-1][*c1+*c2-3]=arr[*r1+*r2-2][*c1+*c2-2]-1;
+             arr[*r1+*r2-1][*c1+*c2-2]=arr[*r1+*r2-3][*c1+*c2-2];
+             (*r2)++;
+             if(*c1>*c2) *c1=*c2;
+             else *c2=*c1;
+             dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+            }
+            else if(arr[*r1+*r2][*c1+*c2-2]!=' '&&arr[*r1+*r2-1][*c1+*c2-1]!=' '&&arr[*r1+*r2-1][*c1+*c2-3]!=' '){
+                (*score)++;
+                 arr[*r1+*r2-1][*c1+*c2-2]=arr[*r1+*r2-3][*c1+*c2-2];
+            }
+        }
+        else if(*r1!=1 &&arr[*r1+*r2-1][*c1+*c2-2]!=' '){
+            if(arr[*r1+*r2-4][*c1+*c2-2]==' '&&arr[*r1+*r2-3][*c1+*c2-1]!=' '&&arr[*r1+*r2-3][*c1+*c2-3]!=' '){
+                 (*z)--;
+             (*moves)++;
+             (*score)++;
+             arr[*r1+*r2-4][*c1+*c2-2]=arr[*r1+*r2-2][*c1+*c2-2];
+             arr[*r1+*r2-3][*c1+*c2-2]=arr[*r1+*r2-3][*c1+*c2-2];
+             (*r1)--;
+             (*r2)--;
+             dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+            }
+            else if(arr[*r1+*r2-4][*c1+*c2-2]!=' '&&arr[*r1+*r2-3][*c1+*c2-1]==' '&&arr[*r1+*r2-3][*c1+*c2-3]!=' '){
+                 (*z)--;
+             (*moves)++;
+             (*score)++;
+             arr[*r1+*r2-3][*c1+*c2-1]=arr[*r1+*r2-2][*c1+*c2-2]-1;
+             arr[*r1+*r2-3][*c1+*c2-2]=arr[*r1+*r2-3][*c1+*c2-2];
+             (*r1)--;
+             if(*c1>*c2) *c2=*c1;
+             else *c1=*c2;
+             dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+            }
+            else if(arr[*r1+*r2-4][*c1+*c2-2]!=' '&&arr[*r1+*r2-3][*c1+*c2-1]!=' '&&arr[*r1+*r2-3][*c1+*c2-3]==' '){
+                 (*z)--;
+             (*moves)++;
+             (*score)++;
+             arr[*r1+*r2-3][*c1+*c2-3]=arr[*r1+*r2-2][*c1+*c2-2]-1;
+             arr[*r1+*r2-3][*c1+*c2-2]=arr[*r1+*r2-3][*c1+*c2-2];
+             (*r1)--;
+             if(*c1>*c2) *c1=*c2;
+             else *c2=*c1;
+             dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+            }
+            else if(arr[*r1+*r2-4][*c1+*c2-2]!=' '&&arr[*r1+*r2-3][*c1+*c2-1]!=' '&&arr[*r1+*r2-3][*c1+*c2-3]!=' '){
+             (*score)++;
+              arr[*r1+*r2-3][*c1+*c2-2]=arr[*r1+*r2-3][*c1+*c2-2];   
+            }
+        }
+    }
+    else if(*c1==*c2){
+      if(*c1!=1&& arr[*r1+*r2-2][*c1+*c2-1!=' ']){
+            if(arr[*r1+*r2-2][*c1+*c2-4]==' '&&arr[*r1+*r2-1][*c1+*c2-3]!=' '&&arr[*r1+*r2-3][*c1+*c2-3]!=' '){
+                 (*z)--;
+             (*moves)++;
+             (*score)++;
+             arr[*r1+*r2-2][*c1+*c2-4]=arr[*r1+*r2-2][*c1+*c2-2];
+             arr[*r1+*r2-2][*c1+*c2-3]=arr[*r1+*r2-2][*c1+*c2-1];
+             (*c1)--;
+             (*c2)--;
+             dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+            }
+            else if(arr[*r1+*r2-2][*c1+*c2-4]!=' '&&arr[*r1+*r2-1][*c1+*c2-3]!=' '&&arr[*r1+*r2-3][*c1+*c2-3]==' '){
+                 (*z)--;
+             (*moves)++;
+             (*score)++;
+             arr[*r1+*r2-3][*c1+*c2-3]=arr[*r1+*r2-2][*c1+*c2-2]+1;
+             arr[*r1+*r2-2][*c1+*c2-3]=arr[*r1+*r2-2][*c1+*c2-1];
+             (*c1)--;
+             if(*r1>*r2) *r1=*r2;
+             else *r2=*r1;
+             dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+            }
+            else if(arr[*r1+*r2-2][*c1+*c2-4]!=' '&&arr[*r1+*r2-1][*c1+*c2-3]==' '&&arr[*r1+*r2-3][*c1+*c2-3]!=' '){
+                 (*z)--;
+             (*moves)++;
+             (*score)++;
+             arr[*r1+*r2-1][*c1+*c2-3]=arr[*r1+*r2-2][*c1+*c2-2]+1;
+             arr[*r1+*r2-2][*c1+*c2-3]=arr[*r1+*r2-2][*c1+*c2-1];
+             (*c1)--;
+             if(*r1>*r2) *r2=*r1;
+             else *r1=*r2;
+             dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+            }
+            else if(arr[*r1+*r2-2][*c1+*c2-4]!=' '&&arr[*r1+*r2-1][*c1+*c2-3]!=' '&&arr[*r1+*r2-3][*c1+*c2-3]!=' '){
+                (*score)++;
+                arr[*r1+*r2-2][*c1+*c2-3]=arr[*r1+*r2-2][*c1+*c2-1];
+            }
+      }
+      else if(*c1!=gameSize+1&& arr[*r1+*r2-2][*c1+*c2-3!=' ']){
+             if(arr[*r1+*r2-2][*c1+*c2]==' '&&arr[*r1+*r2-1][*c1+*c2-1]!=' '&&arr[*r1+*r2-3][*c1+*c2-1]!=' '){
+                 (*z)--;
+             (*moves)++;
+             (*score)++;
+             arr[*r1+*r2-2][*c1+*c2]=arr[*r1+*r2-2][*c1+*c2-2];
+             arr[*r1+*r2-2][*c1+*c2-1]=arr[*r1+*r2-2][*c1+*c2-3];
+             (*c1)++;
+             (*c2)++;
+            dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+             }
+            else if(arr[*r1+*r2-2][*c1+*c2]!=' '&&arr[*r1+*r2-1][*c1+*c2-1]==' '&&arr[*r1+*r2-3][*c1+*c2-1]!=' '){
+                ( *z)--;
+             ( *moves)++;
+             (*score)++;
+             arr[*r1+*r2-3][*c1+*c2-1]=arr[*r1+*r2-2][*c1+*c2-2]+1;
+             arr[*r1+*r2-2][*c1+*c2-1]=arr[*r1+*r2-2][*c1+*c2-3];
+             (*c1)--;
+             if(*r1>*r2) *r2=*r1;
+             else *r1=*r2;
+             dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+            }
+            else if(arr[*r1+*r2-2][*c1+*c2]!=' '&&arr[*r1+*r2-1][*c1+*c2-1]!=' '&&arr[*r1+*r2-3][*c1+*c2-1]==' '){
+                 (*z)--;
+             (*moves)++;
+             (*score)++;
+             arr[*r1+*r2-3][*c1+*c2-1]=arr[*r1+*r2-2][*c1+*c2-2]+1;
+             arr[*r1+*r2-2][*c1+*c2-1]=arr[*r1+*r2-2][*c1+*c2-3];
+             (*c1)--;
+             if(*r1>*r2) *r1=*r2;
+             else *r2=*r1;
+             dfs(gameSize,arr,r1,c1,r2,c2,z,moves,score);
+            }
+            else if(arr[*r1+*r2-2][*c1+*c2]!=' '&&arr[*r1+*r2-1][*c1+*c2-1]!=' '&&arr[*r1+*r2-3][*c1+*c2-1]!=' '){
+                   (*score)++;
+                   arr[*r1+*r2-2][*c1+*c2-1]=arr[*r1+*r2-2][*c1+*c2-3];
+            }
+      }
+    }
+}
---
+++ readLine.h
@@ -0,0 +1,33 @@
+#include <ctype.h>
+
+void readLine(char str[], int n)
+{
+    int ch, i = 0;
+    int prevIsWhitespace = 0; //Flag to track the previous character
+
+    //Skip leading whitespaces
+    while (((ch=getchar()) != '\n') && isspace(ch))
+    {
+        //Do nothing, just skip whitespaces
+    }
+
+    //Read the rest of the line
+    while (ch != '\n')
+    {
+        if (i<n)
+        {
+            //If the previous character was a whitespace and the current character is also a whitespace,
+            //skip storing it in the array
+            if (!(prevIsWhitespace && isspace(ch)))
+            {
+                ch=tolower(ch);
+                str[i++] = ch;
+            }
+
+            //Update the flag based on the current character
+            prevIsWhitespace = isspace(ch);
+        }
+        ch = getchar();
+    }
+    str[i] = '\0'; //sets the last element to be null (to make it a string)
+}
