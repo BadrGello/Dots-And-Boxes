0d3a52f170870c83b7f01b7522bed7cdb5aa55b9
adham165 <adhamanas2004@gmail.com>
2023-12-25 15:23:18 +0200

    v0.81

--- dfs.h
+++ dfs.h
@@ -78,7 +78,7 @@ void openChain(int gameSize,char arr[gameSize*2+1][gameSize*2+1],int r1,int c1,i
              arr[r1+ r2-2][ c1+ c2-3]=arr[r1+ r2-2][ c1+ c2-1];
              if(r1> r2) openChain(gameSize,arr,r2,c1-1,r2,c2,z,moves,score);
              else  openChain(gameSize,arr,r1,c1-1,r1,c2,z,moves,score);
-             
+
             }
             else if(arr[r1+ r2-2][ c1+ c2-4]!=' '&&arr[r1+ r2-1][ c1+ c2-3]==' '&&arr[r1+ r2-3][ c1+ c2-3]!=' '){
              (*z)--;
@@ -136,7 +136,7 @@ else if(r1==r2&&arr[r1+r2-1][c1+c2-2]==' '&&r1!=gameSize+1){
        arr[r1+r2-1][c1+c2-2]='9';
        if(arr[r1+r2][c1+c2-2]!=' ') w=1;
        else w=dfs(gameSize,arr,r1+1,c1,r2+1,c2);
-       if(openChain[r1+r2-1][c2+c2-2]!=' ') y=1;
+       if(arr[r1+r2-1][c2+c2-2]!=' ') y=1;
        else y=dfs(gameSize,arr,r1,c2,r2+1,c2);
        if(arr[r1+r2-1][c1+c1-2]!=' ') z=1;
        else z=dfs(gameSize,arr,r1,c1,r2+1,c1);
@@ -165,27 +165,55 @@ else if(c1==c2&&c1==gameSize+1) y=0;
 int r=(x*y*z*w);
 return r;
 }
-void chainOfBoxes(int gameSize,char arr[2*gameSize+1][2*gameSize+1],int r1,int c1,int r2,int c2,int* z,int* moves,int* Score){
+void chainOfBoxes(int gameSize,char arr[2*gameSize+1][2*gameSize+1],int r1,int c1,int r2,int c2,int* z,int* moves,int* score){
     int i,j;
 if(dfs(gameSize,arr,r1,c1,r2,c2)){
     for(i=0;i<=2*gameSize;i++){
         for(j=0;j<=2*gameSize;j++){
             if(arr[i][j]=='9'){
-            (*z)--;
-            (*moves)++;
-            (*score)++;   
+            (*score)++;
             if(arr[r1+r2-2][c1+c2-2]=='2'||arr[r1+r2-2][c1+c2-2]=='3'){
-            if(arr[i][j+1]==' ') arr[i][j+1]='2';
-            if(arr[i][j-1]==' ') arr[i][j-1]='2';
-            if(arr[i+1][j]==' ') arr[i+1][j]='3';
-            if(arr[i][j+1]==' ') arr[i-1][j]='3';
+            if(arr[i][j+1]==' ') {
+                    arr[i][j+1]='2';
+                    (*z)--;
+                    (*moves)++;
+            }
+            if(arr[i][j-1]==' ') {
+                    arr[i][j-1]='2';
+                    (*z)--;
+                   (*moves)++;
+            }
+            if(arr[i+1][j]==' ') {
+                    arr[i+1][j]='3';
+                    (*z)--;
+                    (*moves)++;
+            }
+            if(arr[i][j+1]==' ') {
+                    arr[i-1][j]='3';
+                    (*z)--;
+                    (*moves)++;
+            }
             arr[i][j]='6';
             }
             else {
-            if(arr[i][j+1]==' ') arr[i][j+1]='4';
-            if(arr[i][j-1]==' ') arr[i][j-1]='4';
-            if(arr[i+1][j]==' ') arr[i+1][j]='5';
-            if(arr[i][j+1]==' ') arr[i-1][j]='5';
+            if(arr[i][j+1]==' ') {
+                    arr[i][j+1]='4';
+                    (*z)--;
+                    (*moves)++;}
+            if(arr[i][j-1]==' ') {
+                    arr[i][j-1]='4';
+                    (*z)--;
+                    (*moves)++;}
+            if(arr[i+1][j]==' ') {
+                    arr[i+1][j]='5';
+                    (*z)--;
+                    (*moves)++;
+            }
+            if(arr[i][j+1]==' ') {
+                    arr[i-1][j]='5';
+                    (*z)--;
+                    (*moves)++;
+            }
             arr[i][j]='7';
             }
             }
