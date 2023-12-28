377e7d239bbc83b57da0efc6ec041c5c11b7d26f
adham165 <adhamanas2004@gmail.com>
2023-12-28 14:48:41 +0200

    0.969

--- save.h
+++ save.h
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 void saveTheGame(int gameSize, char arr[2 * gameSize + 1][2 * gameSize + 1], int playerTurn, char player1[256], char player2[256], int p1Score, int p2Score, int player1Moves, int player2Moves, int z, int dfsFlag, int botFlag, int savedTime) {
     FILE *fb, *fb2;
     char saveSlot = '1', x;
@@ -11,37 +10,43 @@ void saveTheGame(int gameSize, char arr[2 * gameSize + 1][2 * gameSize + 1], int
         do {
             system("cls");
             if (k > 1) {
-                printf(" there is a saved game in this slot Do you want to overwrite it? (y/n)\n");
+                printf("there is a saved game in this slot Do you want to overwrite it?\n");
+                x=' ';
+                while(x!='y'&&x!='Y'&&x!='n'&&x!='N'){
+
+                printf("please enter(y/n):");
                 getchar();
                 scanf("%c", &x);
-                if (x == 'y') {
+
+            }
+             if (x == 'y'||x=='Y'||saveSlot=='r'||saveSlot=='R') {
                     break;
                 }
             }
              system("cls");
-            if (saveSlot - '0' > 3 || saveSlot - '0' <= 0) {
+            if ((saveSlot - '0' > 3 || saveSlot - '0' <= 0)&&saveSlot!='r'&&saveSlot!='R') {
                 printf("Invalid value\n");
             }
 
-            printf("Choose save slot from 1 to 3\n");
+            printf("Choose save slot from 1 to 3(press r to return to the game)\n");
             scanf(" %c", &saveSlot);
-        } while (saveSlot - '0' > 3 || saveSlot - '0' <= 0);
+        } while ((saveSlot - '0' > 3 || saveSlot - '0' <= 0)&&saveSlot!='r'&&saveSlot!='R');
 
         if (saveSlot == '1') {
             fb2 = fopen("saveSlot1.bin", "rb");
-            fb = fopen("saveSlot1.bin", "wb+");
+            fb = fopen("saveSlot1.bin", "w+");
         } else if (saveSlot == '2') {
             fb2 = fopen("saveSlot2.bin", "rb");
-            fb = fopen("saveSlot2.bin", "wb+");
+            fb = fopen("saveSlot2.bin", "w+");
         } else if (saveSlot == '3') {
             fb2 = fopen("saveSlot3.bin", "rb");
-            fb = fopen("saveSlot3.bin", "wb+");
+            fb = fopen("saveSlot3.bin", "w+");
         }
-          if (x == 'y') {
+          if (x == 'y'||x=='Y'||saveSlot=='r'||saveSlot=='R') {
                     break;
                 }
-    } while (fb2 != NULL);
-
+    } while (fb2 != NULL&&saveSlot!='r'&&saveSlot!='R');
+    if(saveSlot!='r'&&saveSlot!='R'){
     char numericalGameData[100];
     sprintf(numericalGameData, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d.",
             gameSize, playerTurn, dfsFlag, botFlag, savedTime, p1Score, p2Score, player1Moves, player2Moves, z);
@@ -56,4 +61,5 @@ void saveTheGame(int gameSize, char arr[2 * gameSize + 1][2 * gameSize + 1], int
 
     fclose(fb);
     fclose(fb2);
+}
 }
